3147201277cfd35f9cf4e116b16b66c965472653
justinmutinta <justinmutinta@hotmail.com>
2024-11-07 19:19:19 -0600

    Able to get the list onto the main page

--- finance/finances/views.py
+++ finance/finances/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+information = ["one", 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+
+
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'information': information})
