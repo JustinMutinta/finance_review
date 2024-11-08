51aad97e06d2769b6cd589722110b7d9d8a246f0
justinmutinta <justinmutinta@hotmail.com>
2024-11-07 19:45:01 -0600

    playing with graphs

--- finance/finances/views.py
+++ finance/finances/views.py
@@ -8,3 +8,14 @@ information = ["one", 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', '
 
 def home(request):
     return render(request, 'home.html', {'information': information})
+
+
+def index(request):
+    data_points = [
+        {"label": "apple", "y": 10},
+        {"label": "orange", "y": 15},
+        {"label": "banana", "y": 25},
+        {"label": "mango", "y": 30},
+        {"label": "grape", "y": 28}
+    ]
+    return render(request, 'index.html', {"data_points": data_points})
